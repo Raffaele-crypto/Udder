abff13353917fa84484069113130084c4d98f0eb
Raffaele Reggiani <raffaele.reggiani@yahoo.com>
2020-10-04 11:29:20 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Raffaele Reggiani on 2020/10/4.
+//  Copyright © 2020 Raffaele Reggiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
